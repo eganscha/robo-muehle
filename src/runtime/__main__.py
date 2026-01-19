703aaeaa7644dce3ca7ee75f7061bd3ed019bada
Eugen Ganscha <eganscha@gmail.com>
2026-01-19 11:47:32 +0100

    movement refactor

--- src/runtime/__main__.py
+++ src/runtime/__main__.py
@@ -1,13 +1,16 @@
-from ai_placeholder import Ai
-from piecewalker.ned2 import Ned2
-from runtime import game_loop
-from yolo_detection.detector import Detector
-
-from .args import parse_args
+from piecewalker import ned2_provider
 
 if __name__ == "__main__":
-    args = parse_args()
-    ned2 = Ned2()
-    ai = Ai(args.model_play)
-    detector = Detector()
-    game_loop.run_game_loop(ned2, detector, ai, args.human_start, args.robot_only)
+    ned2 = ned2_provider.get_ned2()
+
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=False)
+    ned2.move(from_idx=None, to_idx=None, back_to_idle=True)
+
+    ned2_provider.close_ned2()
